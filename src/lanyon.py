f81ca744d51274aa66b273c85f1b2ef0356d7a8b
Ben Keller <malzraa@gmail.com>
2012-08-20 02:16:19 -0400

    Added some basic parameter reading.  Boy, yaml is so simple it is almost boring.

--- src/lanyon.py
+++ src/lanyon.py
@@ -4,9 +4,15 @@ import yaml
 import markdown
 from argparse import ArgumentParser
 
+def build_tex(todo, author, institution):
+
 if __name__ == "__main__"
 	parser = ArgumentParser()
 	(options, args) = parser.parse_args()
 	if len(args) < 1:
 		print "Insufficient arguments:  use --help for more info."
 		exit(1)
+	try:
+		params = yaml.load(open('params.yaml').readlines())
+	except IOError:
+		print "Unable to open parameter file: are you in the main lanyon directory?"
